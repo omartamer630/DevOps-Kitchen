e0058597ad03c2dd4ff5e4b032971bed85055b5a
omar tamer <omartamerdev@gmail.com>
2024-11-30 22:47:47 +0200

    changed ec2 ami and ssm command

--- 09- Week Nine/ec2.tf
+++ 09- Week Nine/ec2.tf
@@ -1,10 +1,15 @@
 resource "aws_instance" "forgtech-ec2" {
-  ami                    = "ami-0866a3c8686eaeeba"
+  ami                    = "ami-0a3c3a20c09d6f377"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.forgtech-public-subnet-a[0].id
   vpc_security_group_ids = [aws_security_group.forgtech-ec2-sg.id]
   key_name = aws_key_pair.forgtech-key-pair.key_name  # Attach key pair here
   associate_public_ip_address = true
-  iam_instance_profile = aws_iam_instance_profile.ec2-profile.arn
+  iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
   tags = var.environment
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+              sudo systemctl start amazon-ssm-agent
+    EOF
 }
