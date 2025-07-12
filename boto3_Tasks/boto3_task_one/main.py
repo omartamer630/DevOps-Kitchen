63e1599de83cc5bd5e7cdd665417019dfad06dff
Omar tamer <120190572+omartamer630@users.noreply.github.com>
2025-07-12 09:11:27 +0300

    Added Docstring in Methods

--- boto3_Tasks/boto3_task_one/main.py
+++ boto3_Tasks/boto3_task_one/main.py
@@ -1,37 +1,50 @@
 import boto3
 
 # Select Region
-region = 'us-east-1'
+REGION = 'us-east-1'
 
-# Create Client 
-ec2_client = boto3.client('ec2', region_name=region)
+# Create Client
+ec2_client = boto3.client('ec2', region_name=REGION)
 
 # Fetch All VPCs Details
 def all_vpcs():
-  available_vpcs = ec2_client.describe_vpcs()
-  return available_vpcs["Vpcs"]
+    """
+    Fetch Every Detail in All VPCs in AWS Account
+    Return All Details in Json
+    """
+    available_vpcs = ec2_client.describe_vpcs()
+    return available_vpcs["Vpcs"]
 
 # Fetch All EC2s Details
 def all_instances():
-  available_ec2 = ec2_client.describe_instances()
-  instances = []
-  select_ec2 = available_ec2["Reservations"]
-  for reservation in select_ec2:
-    for ec2 in reservation["Instances"]:
-        instances.append(ec2)
-  return instances
+    """
+    Fetch Every Detail in EC2s in AWS Account
+    Return All Details in Json
+    """
+    available_ec2 = ec2_client.describe_instances()
+    instances = []
+    select_ec2 = available_ec2["Reservations"]
+    for reservation in select_ec2:
+        for ec2 in reservation["Instances"]:
+            instances.append(ec2)
+    return instances
 
 
-def fetch_into_file(text_name , fetch_resource):
-  with open(text_name, 'a') as file_object:
-    file_object.write(str(fetch_resource))
+def fetch_into_file(text_name = '', fetch_resource = ''):
+    """
+    First Parameter (text_name) = file name
+    Second Parameter (fetch_resource) = file that i will put the fetched data in it
+
+    Fetch Every Detail in All VPCs and EC2 in File text_name
+    """
+    with open(text_name, 'a',  encoding="utf-8") as file_object:
+        file_object.write(str(fetch_resource))
 
 
+VPC_FILE = 'vpc_details.txt'
+EC2_FILE = 'ec2_details.txt'
 
-vpc_content_file = 'vpc_details.txt'
-ec2_content_file = 'ec2_details.txt'
 
+fetch_into_file(VPC_FILE, all_vpcs())
 
-fetch_into_file(vpc_content_file, all_vpcs())
-
-fetch_into_file(ec2_content_file,all_instances())
+fetch_into_file(EC2_FILE, all_instances())
